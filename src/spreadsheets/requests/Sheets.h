d9dc5195a111f6bcc940ed110a4aa80db57267db
unknown <k_suwatchai@hotmail.com>
2024-03-28 19:21:56 +0700

    Update data options.

--- src/spreadsheets/requests/Sheets.h
+++ src/spreadsheets/requests/Sheets.h
@@ -24,73 +24,73 @@
  *                                          DataSourceSheetProperties               DataSourceColumn                    DataSourceColumnReference
  * 
  *                                                                                  DataExecutionStatus                 (enum) DataExecutionState
-                                                                                                                        (enum) DataExecutionErrorCode
-	
-                    GridData                RowData                                 CellData*
-
-					                        DimensionProperties                     DeveloperMetadata                   DeveloperMetadataLocation           (enum) DeveloperMetadataLocationType
-                                                                                                                                                            DimensionRange
-
-                                                                                                                        (enum) DeveloperMetadataVisibility
-
-                                                                                    DataSourceColumnReference
-
-                    GridRange
-                    ConditionalFormatRule   GridRange
-                                            BooleanRule                             BooleanCondition*
-                                                                                    CellFormat*
-
-                                            GradientRule                            InterpolationPoint                  ColorStyle
-                                                                                                                        (enum) InterpolationPointType
-
-                    FilterView              GridRange
-                                            SortSpec*
-                                            FilterSpec*
-
-                    ProtectedRange          GridRange
-                                            Editors
-
-                    BasicFilter             GridRange
-                                            SortSpec*
-                                            FilterSpec*
-
-                    EmbeddedChart           ChartSpec                               TextFormat
-                                                                                    TextPosition
-                                                                                    ColorStyle*
-                                                                                    DataSourceChartProperties
-                                                                                    FilterSpec*
-                                                                                    SortSpec*
-                                                                                    (enum) ChartHiddenDimensionStrategy
-                                                                                    BasicChartSpec*
-                                                                                    PieChartSpec*
-                                                                                    BubbleChartSpec*
-                                                                                    CandlestickChartSpec*
-                                                                                    OrgChartSpec*
-                                                                                    HistogramChartSpec*
-                                                                                    WaterfallChartSpec*
-                                                                                    TreemapChartSpec*
-                                                                                    ScorecardChartSpec*
-
-                                            EmbeddedObjectPosition                  OverlayPosition                             GridCoordinate
-
-                                            EmbeddedObjectBorder                    ColorStyle*
-
-                    BandedRange             GridRange
-                                            BandingProperties                       ColorStyle*
-
-                    DeveloperMetadata       DeveloperMetadataLocation*
-                                            (enum) DeveloperMetadataVisibility
-
-                    DimensionGroup          DimensionRange*
-
-                    Slicer                  SlicerSpec                              GridRange
-                                                                                    FilterCriteria*
-                                                                                    TextFormat
-                                                                                    ColorStyle*
-                                                                                    (enum) HorizontalAlign
-
-                                            EmbeddedObjectPosition                  OverlayPosition
-                                                                                    GridCoordinate*
+ *                                                                                                                      (enum) DataExecutionErrorCode
+ * 	
+ *                  GridData                RowData                                 CellData*
+ * 
+ *                                          DimensionProperties                     DeveloperMetadata                   DeveloperMetadataLocation           (enum) DeveloperMetadataLocationType
+ *                                                                                                                                                          DimensionRange
+ *
+ *                                                                                                                      (enum) DeveloperMetadataVisibility
+ * 
+ *                                                                                  DataSourceColumnReference
+ * 
+ *                  GridRange
+ *                  ConditionalFormatRule   GridRange
+ *                                          BooleanRule                             BooleanCondition*
+ *                                                                                  CellFormat*
+ * 
+ *                                          GradientRule                            InterpolationPoint                  ColorStyle
+ *                                                                                                                      (enum) InterpolationPointType
+ * 
+ *                  FilterView              GridRange
+ *                                          SortSpec*
+ *                                          FilterSpec*
+ * 
+ *                  ProtectedRange          GridRange
+ *                                          Editors
+ * 
+ *                  BasicFilter             GridRange
+ *                                          SortSpec*
+ *                                          FilterSpec*
+ * 
+ *                  EmbeddedChart           ChartSpec                               TextFormat
+ *                                                                                  TextPosition
+ *                                                                                  ColorStyle*
+ *                                                                                  DataSourceChartProperties
+ *                                                                                  FilterSpec*
+ *                                                                                  SortSpec*
+ *                                                                                  (enum) ChartHiddenDimensionStrategy
+ *                                                                                  BasicChartSpec*
+ *                                                                                  PieChartSpec*
+ *                                                                                  BubbleChartSpec*
+ *                                                                                  CandlestickChartSpec*
+ *                                                                                  OrgChartSpec*
+ *                                                                                  HistogramChartSpec*
+ *                                                                                  WaterfallChartSpec*
+ *                                                                                  TreemapChartSpec*
+ *                                                                                  ScorecardChartSpec*
+ * 
+ *                                          EmbeddedObjectPosition                  OverlayPosition                             GridCoordinate
+ * 
+ *                                          EmbeddedObjectBorder                    ColorStyle*
+ * 
+ *                  BandedRange             GridRange
+ *                                          BandingProperties                       ColorStyle*
+ * 
+ *                  DeveloperMetadata       DeveloperMetadataLocation*
+ *                                          (enum) DeveloperMetadataVisibility
+ * 
+ *                  DimensionGroup          DimensionRange*
+ * 
+ *                  Slicer                  SlicerSpec                              GridRange
+ *                                                                                  FilterCriteria*
+ *                                                                                  TextFormat
+ *                                                                                  ColorStyle*
+ *                                                                                  (enum) HorizontalAlign
+ * 
+ *                                          EmbeddedObjectPosition                  OverlayPosition
+ *                                                                                  GridCoordinate*
  * 
 */
 namespace GSHEET
