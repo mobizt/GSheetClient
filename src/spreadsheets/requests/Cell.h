c2a99b1250fd2ed00592d096f33fa68249a7f954
unknown <k_suwatchai@hotmail.com>
2024-03-28 12:19:47 +0700

    Update data options.

--- src/spreadsheets/requests/Cell.h
+++ src/spreadsheets/requests/Cell.h
@@ -69,6 +69,8 @@
  *              |                                                                                   +       ColorStyle
  *              |
  *              +                           DataSourceFormula
+ * 
+ * See PivotTable.h
 */
 
 namespace GSHEET
